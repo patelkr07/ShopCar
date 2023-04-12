6a136cb3b0dd12e43bb9c958b4f6b056303525be
Kunal Patel <patelkr07@gmail.com>
2023-04-12 02:55:26 -0500

    fixed broken image on buy page

--- client/src/pages/Buy.js
+++ client/src/pages/Buy.js
@@ -15,7 +15,7 @@ import ShopcarLogo from "../components/ShopcarLogo"
 
 //removed images that have vague/missing usage rights
 
-const carPixArray = ["https://www.wsupercars.com/wallpapers/Honda/1992-Honda-NSX-R-V4-1600.jpg", "https://images.pexels.com/photos/2127740/pexels-photo-2127740.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260", "https://images.pexels.com/photos/831475/pexels-photo-831475.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" ]
+const carPixArray = ["https://images.pexels.com/photos/14055736/pexels-photo-14055736.jpeg", "https://images.pexels.com/photos/2127740/pexels-photo-2127740.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260", "https://images.pexels.com/photos/831475/pexels-photo-831475.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" ]
 
 // const randomizer2 = Math.floor((carPixArray.length)*Math.random())
 
